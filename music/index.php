dac4eff32f0498b2a6aa7cccca735ae69242dec0
jsm33t <jskainthofficial@gmail.com>
2022-03-02 14:52:33 +0530

    added music page

--- music/index.php
+++ music/index.php
@@ -1,5 +1,5 @@
 <?php
-$title = "Home";
+$title = "Music";
 $description = "Basic flex stuffs and all";
 $logo = "white";
 $home = "active";
@@ -93,6 +93,222 @@ include $_SERVER['DOCUMENT_ROOT'] . "/includes/head.php";
                     </div>
                 </section>
                 <!-- End Call Action Section -->
+
+                </section>
+            <!-- Divider -->
+            <hr class="mt-0 mb-0 "/>
+            <!-- End Divider -->
+            <!-- Section -->
+            <section class="small-section">
+            <div class="container relative">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h3 class="blog-item-title font-alt mb-10"><a href="#">
+                            RELEASES
+                        </a></h3>
+                        <hr class="mt-0 mb-30" />
+                        <!-- Blog Post -->
+                        <div class="row">
+                        <div class="col-md-6">
+                            <!-- Media Gallery -->
+                            <div class="blog-media">
+                                <ul class="clearlist content-slider">
+                                    <li>
+                                    <a href="#"><img src="/music/melancholy/promo.png" alt="" /></a>
+                                    </li>
+                                    <li>
+                                    <a href="#"><img src="/music/melancholy/promo_b.png" alt="" /></a>
+                                    </li>
+                                    <li>
+                                    <a href="#"><img src="/music/melancholy/promo_c.png" alt="" /></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-30">
+                            <!-- Post Title -->
+                            <h2 class="post-prev-title font-alt mt-0"><a href="blog-single-sidebar-right.html">MELCNCHOLY</a></h2>
+                            <!-- Author, Categories, Comments -->
+                            <div class="blog-item-data mb-20">
+                                <a href="#"><i class="fa fa-clock-o"></i>15 JAN 2020</a>
+                                <span class="separator">&nbsp;</span>
+                                <a href="#"><i class="fa fa-comments"></i> jsm33t </a>
+                            </div>
+                            <!-- Text Intro -->
+                            <div class="post-prev-text">
+                            Melancholy is a snapshot of the trajectory of an offbeat mind. The graticule of a depressed mind remains a mystery.
+                            Experience the ambience of Melancholy
+                            </div>
+                            <!-- Read More Link -->
+                            <div class="blog-item-foot">
+                                <a href="/music/melancholy" target="_blank" class="btn btn-mod btn-round  btn-small">SOURCE CODE <i class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                        </div>
+                        <h3 class="blog-item-title font-alt mb-10"><a href="#">Originals</a></h3>
+                        <hr class="mt-0 mb-30" />
+                        <div class="row mb-70">
+                        <div class="col-sm-6">
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-1.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="#" title="">Melancholy (original)</a>
+                                    - jsm33t 
+                                </div>
+                            </div>
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-2.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="/apps/urban_dict" title="">Dhadak (orch teaser)</a>
+                                    Posted by John Doe 7 Mar 
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-3.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="#" title="">Bekhayaali (orch teaser)</a>
+                                    - jsm33t
+                                </div>
+                            </div>
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-4.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="#" title="">Survivors(orch teaser)</a>
+                                    - jsm33t
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+
+
+                        <h3 class="blog-item-title font-alt mb-10"><a href="#">BOOTLEGS</a></h3>
+                        <hr class="mt-0 mb-30" />
+                        <div class="row mb-70">
+                        <div class="col-sm-6">
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-1.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="#" title="">Yotube Downloader | BETA | limit 200/day</a>
+                                    - jsm33t 
+                                </div>
+                            </div>
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-2.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="/apps/urban_dict" title="">Urban Dictionary | API | BETA</a>
+                                    Posted by John Doe 7 Mar 
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-3.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="#" title="">Google Dork Tool</a>
+                                    - jsm33t
+                                </div>
+                            </div>
+                            <div class="blog-post-prev-small clearfix">
+                                <a href=""><img src="images/blog/previews/post-prev-4.jpg" alt="" class="widget-posts-img" /></a>
+                                <div class="widget-posts-descr">
+                                    <a href="#" title="">Hipster’s Style in Web Design and Logo</a>
+                                    Posted by John Doe 7 Mar 
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+
+
+                    </div>
+                    <div class="col-md-4 sidebar">
+                        <!-- Widget -->
+                        <div class="widget">
+                        <h5 class="widget-title font-alt">SOCIALS</h5>
+                        <div class="widget-body">
+                            <a href="#" class="btn btn-mod btn-small btn-round btn-gray mb-10"><i class="fa fa-github"></i>&nbsp;Github</a>
+                            <a href="#" class="btn btn-mod btn-small btn-round btn-gray mb-10"><i class="fa fa-twitter"></i> Twitter</a>
+                            <a href="#" class="btn btn-mod btn-small btn-round btn-gray mb-10"><i class="fa fa-rss"></i> RSS</a>
+                        </div>
+                        </div>
+                        <!-- End Widget -->
+                        <!-- Search Widget -->
+                        <div class="widget">
+                        <form class="form-inline form">
+                            <div class="search-wrap">
+                                <button class="search-button animate" type="submit" title="Start Search">
+                                <i class="fa fa-search"></i>
+                                </button>
+                                <input type="text" class="form-control search-field" placeholder="Search...">
+                            </div>
+                        </form>
+                        </div>
+                        <!-- End Search Widget -->
+                        <!-- Widget -->
+                        <div class="widget">
+                        <h5 class="widget-title font-alt">Contributers</h5>
+                        <div class="widget-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <ul class="clearlist widget-menu">
+                                    <li>
+                                        <a href="https://github.com/jsm33t" target="_blankte" title="">jsm33t</a>
+                                        <small>
+                                        - 7
+                                        </small>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="">Mandy</a>
+                                        <small>
+                                        - 15
+                                        </small>
+                                    </li>
+                                  
+                                    </ul>
+                                </div>
+                                <div class="col-md-6">
+                                    <ul class="clearlist widget-menu">
+                                    <li>
+                                        <a href="https://github.com/TheCuddleDoodle" target="_blank" title="">TheCuddleDoodle</a>
+                                        <small>
+                                        - 3
+                                        </small>
+                                    </li>
+                                   
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <!-- End Widget -->
+                        <!-- Widget -->
+                        <div class="widget">
+                        <h5 class="widget-title font-alt">Tags</h5>
+                        <div class="widget-body">
+                            <div class="tags">
+                               
+                                <a href="">UI & UX</a>
+                                <a href="">Love</a>
+                            </div>
+                        </div>
+                        </div>
+                        <!-- End Widget -->
+           
+                        <!-- Search Widget -->
+                        <div class="widget">
+                        <div class="align-center">
+                            <a href="#"><img src="images/banners/banner-300x250.png" alt="" /></a>
+                        </div>
+                        </div>
+                        <!-- End Search Widget -->
+                    </div>
+                </div>
+            </div>
+            </section>
+            <!-- End Section -->
     </main>
       
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/includes/foot.php"; ?>
