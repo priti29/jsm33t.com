a15b710cd30a9a8672a759e3ed224f397c970221
jsm33t <jskainthofficial@gmail.com>
2022-03-13 07:35:34 +0530

    dark footer fix

---
+++ includes/foot_dark.php
@@ -0,0 +1,88 @@
+            <footer class="page-section bg-dark footer pb-60">
+                <div class="container">
+                    
+                  
+                    <div class="local-scroll mb-30 wow fadeInUp" data-wow-duration="1.2s">
+                        <a href="#top"><img src="/resources/images/j_logo_white.svg" width="78" height="36" alt="Company logo" /><span class="sr-only">Scroll to the top of the page</span></a>
+                    </div>
+                 
+                    <div class="footer-social-links mb-110 mb-xs-60">
+                     <a href="https://instagram.com/jsm33t" title="Instagram" target="_blank"><i class="fa fa-instagram bg-dark"></i></a>
+                     <a href="https://youtube.com/jsm33tmusic" title="Youtube" target="_blank"><i class="fa fa-youtube bg-dark"></i></a>
+                     <a href="https://soundcloud.com/jsm33t" title="SoundCloud" target="_blank"><i class="fa fa-soundcloud bg-dark"></i></a>
+                     <a href="https://open.spotify.com/artist/1y5ovVm8Z5QNKv3NSeQla5" title="Spotify" target="_blank"><i class="fa fa-spotify bg-dark"></i></a> 
+                    </div>
+                    <div class="footer-text">
+                        <div class="footer-copy font-alt">
+                            JSM33T / <?php echo date("d.m.y") ?>
+                        </div>
+                        <div class="footer-made">
+                            Made with &nbsp;<i class="fa fa-heart"></i>&nbsp; for you all.
+                        </div>
+                    </div>
+                </div>
+                <!-- Top Link -->
+                <div class="local-scroll">
+                     <a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+                </div>
+                <!-- End Top Link -->
+                 
+            </footer>
+            
+    <svg class="cursor" width="150" height="150" viewBox="0 0 220 220">
+        <defs>
+            <filter id="filter-1" x="-50%" y="-50%" width="200%" height="200%" filterUnits="objectBoundingBox">
+                <feTurbulence type="fractalNoise" baseFrequency="0" numOctaves="1" result="warp"></feTurbulence>
+                <feOffset dx="-30" result="warpOffset"></feOffset>
+                <feDisplacementMap xChannelSelector="R" yChannelSelector="G" scale="30" in="SourceGraphic" in2="warpOffset"></feDisplacementMap>
+            </filter>
+        </defs>
+        <circle class="cursor__inner" cx="110" cy="110" r="60"></circle>
+    </svg>
+    
+            <!--
+            t <a href="http://themeforest.net/item/rhythm-multipurpose-onemulti-page-template/10140354?ref=Bestlooker" target="_blank" class="buy-button"><i class="fa fa-search" aria-hidden="true"></i> <span>Buy</span></a>
+            -->
+        
+        
+        </div>
+        <script>
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+</script>
+		<script type="text/javascript" src="/resources/js/maincur.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="/resources/js/bootstrap.bundle.min.js"></script>        
+        <script type="text/javascript" src="/resources/js/SmoothScroll.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.scrollTo.min.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.localScroll.min.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.viewport.mini.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.countTo.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.appear.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.sticky.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.parallax-1.1.3.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.fitvids.js"></script>
+        <script type="text/javascript" src="/resources/js/owl.carousel.min.js"></script>
+        <script type="text/javascript" src="/resources/js/isotope.pkgd.min.js"></script>
+        <script type="text/javascript" src="/resources/js/imagesloaded.pkgd.min.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="/resources/js/wow.min.js"></script>
+        <script type="text/javascript" src="/resources/js/masonry.pkgd.min.js"></script>
+        <script type="text/javascript" src="/resources/js/morphext.js"></script>
+        <script type="text/javascript" src="/resources/js/jquery.lazyload.min.js"></script>
+        <script type="text/javascript" src="/resources/js/all.js"></script>
+        <script type="text/javascript" src="/resources/js/contact-form.js"></script>
+        <script>var loc=window.location.href+"";0==loc.indexOf("http://")&&(window.location.href=loc.replace("http://","https://"))</script>
+        <!-- <script type="text/javascript" src="/resources/js/jquery.ajaxchimp.min.js"></script>        -->
+        <!--[if lt IE 10]><script type="text/javascript" src="/resources/js/placeholder.js"></script><![endif]-->
+        
+    </body>
+</html>
