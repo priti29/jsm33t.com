5ed6a068db2cbabb5f3584ab02960ad9016185cb
jsm33t <jskainthofficial@gmail.com>
2022-03-14 15:12:19 +0530

    check

--- login/db_connect.php
+++ login/db_connect.php
@@ -2,7 +2,7 @@
 /* Database connection start */
 $servername = "localhost";
 $username = "jsmtcom_jsm33t";
-$password = "jassikainth";
+$password = "Jsm33t@boi";
 $dbname = "jsmtcom_test";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
