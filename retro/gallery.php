37c5534eedd300d32acac6734a4ce3f7b0f8d7bd
jsm33t <jskainthofficial@gmail.com>
2022-03-29 15:44:57 +0530

    commit

--- retro/gallery.php
+++ retro/gallery.php
@@ -1,70 +1,47 @@
 
-<?php
-
- include('header.php');
-?>
+<?php include('header.php'); ?>
 
 <div class="main-content" id="<?php echo empty($_GET['page'])?'pageContent':''; ?>">
 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4">Gallery</h1>
-        <p class="lead">Yet to come</p>
-    </div>
-
-    <!-- OTHER-->
-    <div class="container">
-        <div class="card mb-4">
-            <div class="card-header">
-                <h4 class="my-0 font-weight-normal">Pics Stuff</h4>
-            </div>
-            <div class="card-body">
-                
-                <a class="btn btn-primary " href="video-background.html">
-                    <img src="assets/icons/video_-0.png" class="icon-16">text random
-                </a>
-                
-				
-				
-
-            </div>
-        </div>
-    </div>
-     <!-- OTHER-->
-
-     <div class="container">
-        <div class="card mb-4">
-            <div class="card-header">
-                <h4 class="my-0 font-weight-normal">Pics Stuff</h4>
-            </div>
-            <div class="card-body">
-                
-                <a class="btn btn-primary " href="video-background.html">
-                    <img src="assets/icons/video_-0.png" class="icon-16">text random
-                </a>
-                
-				
-				
-
-            </div>
-          
-        </div>
-    </div>
-
-     <footer class="taskbar">
-      <div class="row" style="margin-right: 0px;">
-          <div class="col-8">
-          <button class="btn start-button" onclick="crt()"><img src="../assets/icons/fs.png" class="icon-16"> CRT Mode</button>
-              <button class="btn start-button" onclick="openFullscreen();"><img src="../assets/icons/fs.png" class="icon-16">Open Fullscreen</button>
-            <button class="btn start-button" onclick="closeFullscreen();"><img src="../assets/icons/no_fs.png" class="icon-16">Close Fullscreen</button>
-           
-          </div>
-          <div class="col-4 time">Side</div>
+   <h1 class="display-4">Gallery</h1>
+   <p class="lead">Yet to come</p>
+</div>
+<!-- OTHER-->
+<div class="container">
+   <div class="card mb-4">
+      <div class="card-header">
+         <h4 class="my-0 font-weight-normal">Pics Stuff</h4>
       </div>
- 
-  </footer>
-
-
-
-
+      <div class="card-body">
+         <a class="btn btn-primary " href="video-background.html">
+         <img src="assets/icons/video_-0.png" class="icon-16">text random
+         </a>
+      </div>
+   </div>
+</div>
+<!-- OTHER-->
+<div class="container">
+   <div class="card mb-4">
+      <div class="card-header">
+         <h4 class="my-0 font-weight-normal">Pics Stuff</h4>
+      </div>
+      <div class="card-body">
+         <a class="btn btn-primary " href="video-background.html">
+         <img src="assets/icons/video_-0.png" class="icon-16">text random
+         </a>
+      </div>
+   </div>
+</div>
+<footer class="taskbar">
+   <div class="row" style="margin-right: 0px;">
+      <div class="col-8">
+         <button class="btn start-button" onclick="crt()"><img src="../assets/icons/fs.png" class="icon-16"> CRT Mode</button>
+         <button class="btn start-button" onclick="openFullscreen();"><img src="../assets/icons/fs.png" class="icon-16">Open Fullscreen</button>
+         <button class="btn start-button" onclick="closeFullscreen();"><img src="../assets/icons/no_fs.png" class="icon-16">Close Fullscreen</button>
+      </div>
+      <div class="col-4 time">Side</div>
+   </div>
+</footer>
 
 <?php include('footer.php'); ?>
 
