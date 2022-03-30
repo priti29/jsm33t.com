4fd216c369dc3fa9faf82f477d06485da093f931
jsm33t <jskainthofficial@gmail.com>
2022-03-30 17:12:32 +0530

    force https retro mode

--- retro/footer.php
+++ retro/footer.php
@@ -108,6 +108,7 @@ function crt()
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script type="text/javascript" src="redirect-ajax.js"></script>
+<script>var loc=window.location.href+"";0==loc.indexOf("http://")&&(window.location.href=loc.replace("http://","https://"))</script>
 </body>
 </html>     
 <?php
